f1ef7a869452338ab401d31dc65f3d4a4f77b51a
Bryce Thuilot <bthuilot@gmail.com>
2019-02-09 14:07:42 -0500

    Beginning to Create contact page

---
+++ sidebar-templates/contact-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Contact Page
+ * Created by Bryce Thuilot
+ *
+ * @package understrap
+ */
+get_header();
+
+$container   = get_theme_mod( 'understrap_container_type' );
+?>
+<div class="padding-top-bottom">
+</div>
+<div class="container">
+    <h2 id="contact-header"><span>contact us.</span></h2>
+
+</div>
