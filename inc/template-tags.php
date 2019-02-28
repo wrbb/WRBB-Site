6663ab72a8c2417b381d9bbdfec07c1c67bb884b
Spencer LaChance <spencerlachance25@gmail.com>
2019-02-28 15:22:56 -0500

    Got rid of stupid date updated in parentheses

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -24,14 +24,14 @@ if ( ! function_exists ( 'understrap_posted_on' ) ) {
 			esc_html( get_the_modified_date() )
 		);
 		$posted_on = sprintf(
-			esc_html_x( '%s', 'post date', 'understrap' ),
+			esc_html_x( 'Posted on %s', 'post date', 'understrap' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 		$byline = sprintf(
 			esc_html_x( 'by %s', 'post author', 'understrap' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
-		echo '<span class="byline"> ' . $byline . '</span><span style="color: red"> | </span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 	}
 }
 
