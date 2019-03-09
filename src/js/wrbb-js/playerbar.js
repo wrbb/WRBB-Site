9f52912b7660c3487713881e23b3dcc0caeadcec
Bryce Thuilot <bthuilot@gmail.com>
2019-03-09 14:21:51 -0500

    Fixing playerbar js

--- src/js/wrbb-js/playerbar.js
+++ src/js/wrbb-js/playerbar.js
@@ -5,63 +5,65 @@ var playing = false;
 jQuery(document).ready(function() {
     audio = new Audio('http://129.10.161.130:8000/');
     audio.volume = 0.5;
-})
 
-jQuery( "#playButton" ).click(function() {
- if(playing) {
-     pauseAudio();
- } else {
-     playAudio();
- }
+    jQuery("#playButton").click(function() {
+        if (playing) {
+            pauseAudio();
+        } else {
+            playAudio();
+        }
+    });
+
+    var expanded = false;
+
+    jQuery(document).on('click', '#expand-collapse', function(e){
+        if (expanded) {
+            jQuery("#expand-collapse").html('<i class="fa fa-caret-square-o-up fa-lg" aria-hidden="true"></i>');
+            expanded = false;
+            jQuery('.volume-controls').css('margin', '0');
+        } else {
+            expanded = true;
+            jQuery("#expand-collapse").html('<i class="fa fa-caret-square-o-down fa-lg" aria-hidden="true"></i>');
+            jQuery("#player-icon").width(jQuery(".playerbar").width() * 0.8);
+            jQuery('.volume-controls').css('margin', (jQuery('.player-play-pause').outerWidth(true) / 2) + 'px');
+        }
+        jQuery('.player-controls').toggleClass('show');
+        jQuery('.player-play-pause').toggleClass('show');
+        jQuery('#player-icon').toggleClass('show');
+        jQuery('.playerbar').toggleClass('expand');
+        jQuery('.player-text').toggleClass('show');
+        jQuery('#player-header').toggleClass('show');
+        jQuery('#player-desc').toggleClass('show');
+        console.log('we');
+    });
+
+    function pauseAudio() {
+        audio.volume = 0;
+        playing = false;
+        document.getElementById("playButton").innerHTML = '<i class="fa fa-play-circle-o fa-lg" aria-hidden="true"></i>'
+    }
+
+    function playAudio() {
+        audio.play();
+        playing = true;
+        setAudioLevel(document.getElementById('volume-slider').value / 100);
+        document.getElementById("playButton").innerHTML = '<i class="fa fa-pause-circle-o fa-lg" aria-hidden="true"></i>'
+    }
+
+    function setAudioLevel(audioLevel){
+        if (audioLevel > 1 || audioLevel < 0) {
+            return;
+        }
+        audio.volume = audioLevel;
+    }
+
+    let slider = jQuery('#volume-slider');
+
+    slider.on('input propertychange', function () {
+        if (!playing) {
+            return;
+        }
+        setAudioLevel(this.value / 100);
+    });
+
 });
-
-var expanded = false;
-
-document.querySelector('#expand-collapse').addEventListener('click', function() {
-    if(expanded) {
-        document.getElementById("expand-collapse").innerHTML = '<i class="fa fa-caret-square-o-up fa-lg" aria-hidden="true"></i>'
-        expanded = false;
-        jQuery('.volume-controls').css('margin','0');
-    } else {
-        expanded = true;
-        document.getElementById("expand-collapse").innerHTML = '<i class="fa fa-caret-square-o-down fa-lg" aria-hidden="true"></i>'
-        jQuery("#player-icon").width(jQuery(".playerbar").width() * 0.8);
-        jQuery('.volume-controls').css('margin', (jQuery('.player-play-pause').outerWidth(true)/2) + 'px');
-    }
-    document.querySelector('.player-controls').classList.toggle('show');
-    document.querySelector('.player-play-pause').classList.toggle('show');
-    document.querySelector('#player-icon').classList.toggle('show');
-    document.querySelector('.playerbar').classList.toggle('expand');
-    document.querySelector('.player-text').classList.toggle('show');
-    document.querySelector('#player-header').classList.toggle('show');
-    document.querySelector('#player-desc').classList.toggle('show');
-});
-
-let pauseAudio = () => {
-    audio.volume = 0;
-    playing = false;
-    document.getElementById("playButton").innerHTML = '<i class="fa fa-play-circle-o fa-lg" aria-hidden="true"></i>'
-}
-
-let playAudio = () => {
-    audio.play();
-    playing = true;
-    setAudioLevel(document.getElementById('volume-slider').value / 100);
-    document.getElementById("playButton").innerHTML = '<i class="fa fa-pause-circle-o fa-lg" aria-hidden="true"></i>'
-}
-
-let setAudioLevel = (audioLevel) => {
-    if(audioLevel > 1 || audioLevel < 0) {
-        return;
-    }
-    audio.volume = audioLevel;
-}
-
-let slider = document.getElementById('volume-slider');
-
-slider.oninput = function() {
-    if(!playing) {
-        return;
-    }
-    setAudioLevel(this.value / 100);
-}
