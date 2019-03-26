c1d49d04e54162e56a3fe2863fa2a7e57904fdd5
Spencer LaChance <spencerlachance25@gmail.com>
2019-03-26 16:56:55 -0400

    fixed the posted_on functions

--- functions.php
+++ functions.php
@@ -201,10 +201,38 @@ function my_single_template($single) {
 }
 
 /**
+ * Used for articles
  * Prints HTML with meta information for the current post-date/time and author.
  * Overrides function in inc/template-tags.php
  */
 function understrap_posted_on() {
+  $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+  if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+    $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+  }
+  $time_string = sprintf( $time_string,
+    esc_attr( get_the_date( 'c' ) ),
+    esc_html( get_the_date() ),
+    esc_attr( get_the_modified_date( 'c' ) ),
+    esc_html( get_the_modified_date() )
+  );
+  $posted_on = sprintf(
+    esc_html_x( '%s', 'post date', 'understrap' ),
+    '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+  );
+  $byline = sprintf(
+    esc_html_x( 'by %s', 'post author', 'understrap' ),
+    '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+  );
+  echo '<p>' . $byline . '<span style="color: red"> | </span>' . $posted_on . '</p>'; // WPCS: XSS OK.
+}
+
+/**
+ * Used for article thumbnails on the home page
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Overrides function in inc/template-tags.php
+ */
+function mp_understrap_posted_on() {
   $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
   if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
     $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
