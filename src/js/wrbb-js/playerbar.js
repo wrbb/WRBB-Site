df21081c9d3a9a9eefcf9a4cdbc95fba831deeb4
Bryce Thuilot <bthuilot@gmail.com>
2019-11-14 18:48:54 -0500

    Fix link

--- src/js/wrbb-js/playerbar.js
+++ src/js/wrbb-js/playerbar.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(() => {
     jQuery(".play-button--toast").on("click", () => {
-        window.open('/wordpress/playerbar-popup', 'win2',
+        window.open('/playerbar-popup', 'win2',
             'status=no,toolbar=no,scrollbars=no,titlebar=no,menubar=no,width=300,height=150,' +
             'directories=no,location=no');
     });
