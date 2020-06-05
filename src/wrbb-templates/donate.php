6850fb9b7a57830c401f3d6170001a7e4f576295
Chris Sims <chrissims@Chriss-MacBook-Pro.local>
2020-06-04 21:45:16 -0400

    adding text and button

---
+++ src/wrbb-templates/donate.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Contact Page
+ *
+ * @author Chris Sims
+ * @package understraps
+ */
+
+get_header();
+?>
+
+<div class="padding-top-bottom" xmlns="http://www.w3.org/1999/html">
+    <h2 id="contact-header"><span>donate.</span></h2>
+</div>
+
+<div class="container-fluid">
+    <div class="row justify-content-center padding-top-bottom_small">
+        <div class="col-10">
+            <p class="donate-text">WRBB 104.9FM started out its life as WNEU in December of 1962, and for nearly 60 years, we continue to be a cornerstone of music culture at Northeastern University. In its current iteration, WRBB is still a completely student-run and freeform radio station that strives to offer students and members of the community with the best in independent radio. With nearly 200 students involved in a multitude of functions ranging from broadcasting to graphic design, WRBB is an interdisciplinary hub where students who love music can come together as one. Consider making a donation today to help fund our passion for music, radio, and much more</p>
+        </div>
+    </div>
+
+
+    <div class="row justify-content-center padding-top-bottom">
+        <button type="submit" class="btn btn-danger">Donate</button>
+    </div>
+</div>
+
+<?php get_footer() ?>
