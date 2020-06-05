cfac2c6c7598726b79ae7a68d9db0d66d1fcdcdd
Chris Sims <chrissims@Chriss-MacBook-Pro.local>
2020-06-05 13:42:02 -0400

    button styling 1

--- src/wrbb-templates/donate.php
+++ src/wrbb-templates/donate.php
@@ -10,19 +10,27 @@ get_header();
 ?>
 
 <div class="padding-top-bottom" xmlns="http://www.w3.org/1999/html">
-    <h2 id="contact-header"><span>donate.</span></h2>
+    <h2 id="donate-header"><span>donate.</span></h2>
+
 </div>
 
 <div class="container-fluid">
     <div class="row justify-content-center padding-top-bottom_small">
         <div class="col-10">
-            <p class="donate-text">WRBB 104.9FM started out its life as WNEU in December of 1962, and for nearly 60 years, we continue to be a cornerstone of music culture at Northeastern University. In its current iteration, WRBB is still a completely student-run and freeform radio station that strives to offer students and members of the community with the best in independent radio. With nearly 200 students involved in a multitude of functions ranging from broadcasting to graphic design, WRBB is an interdisciplinary hub where students who love music can come together as one. Consider making a donation today to help fund our passion for music, radio, and much more</p>
+            <p class="donate-text">
+                WRBB 104.9FM started out its life as WNEU in December of 1962, and for nearly 60 years, we continue to be a cornerstone of music culture at Northeastern University. In its current iteration, WRBB is still a completely student-run and freeform radio station that strives to offer students and members of the community with the best in independent radio.
+            </p>
+            <p class="donate-text">
+                With nearly 200 students involved in a multitude of functions ranging from broadcasting to graphic design, WRBB is an interdisciplinary hub where students who love music can come together as one. Consider making a donation today to help fund our passion for music, radio, and much more.<br>
+            </p>
         </div>
     </div>
 
 
     <div class="row justify-content-center padding-top-bottom">
-        <button type="submit" class="btn btn-danger">Donate</button>
+        <a href="https://giving.northeastern.edu/live/profiles/440-club-wrbb">
+            <button id="donate-button" type="submit" class="btn btn-danger">Donate</button>
+        </a>
     </div>
 </div>
 
