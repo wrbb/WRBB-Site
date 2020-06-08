0f1fa779430ee30407f5c4dc555e31e468ec7fc2
Spencer LaChance <lachance.s@husky.neu.edu>
2020-06-08 15:17:15 -0400

    final tweaks

--- src/wrbb-templates/donate.php
+++ src/wrbb-templates/donate.php
@@ -7,16 +7,18 @@
  */
 
 get_header();
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<div class="padding-top-bottom" xmlns="http://www.w3.org/1999/html">
-    <h2 id="donate-header"><span>support.</span></h2>
+<div class="wrapper" id="full-width-page-wrapper">
 
-</div>
+    <div class="<?php echo esc_attr($container); ?>" id="content">
 
-<div class="container-fluid">
-    <div class="row justify-content-center padding-top-bottom_small">
-        <div class="col-10">
+        <hr class="header-line">
+
+        <h2 class="entry-header"><span class="article-title">support.</span></h2>
+
+        <div class="padding-top-bottom_small">
             <p class="donate-text">
                 WRBB 104.9FM started out its life as WNEU in December of 1962, and for nearly 60 years, we continue to be a cornerstone of music culture at Northeastern University. In its current iteration, WRBB is still a completely student-run and freeform radio station that strives to offer students and members of the community with the best in independent radio.
             </p>
@@ -24,10 +26,7 @@ get_header();
                 With nearly 200 students involved in a multitude of functions ranging from broadcasting to graphic design, WRBB is an interdisciplinary hub where students who love music can come together as one. Consider making a donation today to help fund our passion for music, radio, and much more.<br>
             </p>
         </div>
-    </div>
 
-
-    <div class="row justify-content-center padding-top-bottom">
         <a href="https://giving.northeastern.edu/live/profiles/440-club-wrbb">
             <button id="donate-button">Donate</button>
         </a>
