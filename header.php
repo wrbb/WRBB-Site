b780921a0490afa5cdaafad0df81657732ad60a5
Spencer LaChance <lachance.s@husky.neu.edu>
2020-06-08 17:15:23 -0400

    Static header skeleton integrated

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
  */
 
 $container = get_theme_mod('understrap_container_type');
+$template_url = get_bloginfo('template_url');
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -39,138 +40,111 @@ $container = get_theme_mod('understrap_container_type');
 
 <div class="hfeed site" id="page">
     <div class="header">
-        <div class="head-img">
-            <img src="<?php bloginfo('template_url'); ?>/black-w-red.png" id="wrbb-head-logo">
+        <div class="container-fluid wrbb-navbar">
+            <div class="row justify-content-center">
+                <div class="col text-center">
+                    <span class="dropdown-title">
+                        on-air.
+                        <img alt="down arrow" class="down-arrow" src="<?php echo $template_url; ?>/src/img/down-arrow-red.svg"/>
+                    </span>
+                </div>
+                <div class="col text-center">
+                    <span class="dropdown-title">
+                        reviews.
+                        <img alt="down arrow" class="down-arrow" src="<?php echo $template_url; ?>/src/img/down-arrow-red.svg"/>
+                    </span>
+                </div>
+                <div class="col text-center">
+                    <span class="dropdown-title">
+                        music.
+                        <img alt="down arrow" class="down-arrow" src="<?php echo $template_url; ?>/src/img/down-arrow-red.svg"/>
+                    </span>
+                </div>
+                <div class="header-logo">
+                    <a href="<?php echo esc_url(home_url('/')); ?>">
+                        <img alt="white and red logo" src="<?php echo $template_url; ?>/src/img/white-w-red.png"/>
+                    </a>
+                </div>
+                <div class="col text-center">
+                    <span class="dropdown-title">
+                        photos.
+                        <img alt="down arrow" class="down-arrow" src="<?php echo $template_url; ?>/src/img/down-arrow-red.svg"/>
+                    </span>
+                </div>
+                <div class="col text-center">
+                    <span class="dropdown-title">
+                        podcasts.
+                        <img alt="down arrow" class="down-arrow" src="<?php echo $template_url; ?>/src/img/down-arrow-red.svg"/>
+                    </span>
+                </div>
+                <div class="col text-center">
+                    <span class="dropdown-title">
+                        more.
+                        <img alt="down arrow" class="down-arrow" src="<?php echo $template_url; ?>/src/img/down-arrow-red.svg"/>
+                    </span>
+                </div>
+            </div>
         </div>
-        <div class="menu-bar">
-            <div class="left-side row">
-                <div class="col-xs-4 menu-link"><span class="menu-link-name">on air.</span>
-                    <ul class="options">
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
 
-                    </ul>
-                </div>
-                <div class="col-xs-4 menu-link"><span class="menu-link-name">reviews.</span>
-                    <ul class="options">
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                    </ul>
-                </div>
-                <div class="col-xs-4 menu-link"><span class="menu-link-name">music.</span>
-                    <ul class="options">
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="right-side row">
-                <div class="col-xs-4 menu-link"><span class="menu-link-name">photos.</span>
-                    <ul class="options">
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-
-                    </ul>
-                </div>
-                <div class="col-xs-4 menu-link"><span class="menu-link-name">podcasts.</span>
-                    <ul class="options">
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                    </ul>
-                </div>
-                <div class="col-xs-4 menu-link"><span class="menu-link-name">more.</span>
-                    <ul class="options">
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                        <li>Something</li>
-                    </ul>
-                </div>
-            </div>
-
-        </div> <!-- menu-bar -->
         <!-- ******************* The Navbar Area ******************* -->
-        <div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
-
-            <a class="skip-link screen-reader-text sr-only"
-               href="#content"><?php esc_html_e('Skip to content', 'understrap'); ?></a>
-
-            <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-
-                <?php if ('container' == $container) : ?>
-                <div class="container">
-                    <?php endif; ?>
-
+<!--        <div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">-->
+<!---->
+<!--            <a class="skip-link screen-reader-text sr-only"-->
+<!--               href="#content">--><?php //esc_html_e('Skip to content', 'understrap'); ?><!--</a>-->
+<!---->
+<!--            <nav class="navbar navbar-expand-md navbar-dark bg-primary">-->
+<!---->
+<!--                --><?php //if ('container' == $container) : ?>
+<!--                <div class="container">-->
+<!--                    --><?php //endif; ?>
+<!---->
                     <!-- Your site title as branding in the menu -->
-                    <?php if (!has_custom_logo()) { ?>
-
-                <?php if (is_front_page() && is_home()) : ?>
-
-                    <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url(home_url('/')); ?>"
-                                                     title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"
-                                                     itemprop="url"><?php bloginfo('name'); ?></a></h1>
-
-                <?php else : ?>
-
-                    <a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>"
-                       title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>"
-                       itemprop="url"><?php bloginfo('name'); ?></a>
-
-                <?php endif; ?>
-
-
-                    <?php } else {
-                        the_custom_logo();
-                    } ?><!-- end custom logo -->
-
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
+<!--                    --><?php //if (!has_custom_logo()) { ?>
+<!---->
+<!--                --><?php //if (is_front_page() && is_home()) : ?>
+<!---->
+<!--                    <h1 class="navbar-brand mb-0"><a rel="home" href="--><?php //echo esc_url(home_url('/')); ?><!--"-->
+<!--                                                     title="--><?php //echo esc_attr(get_bloginfo('name', 'display')); ?><!--"-->
+<!--                                                     itemprop="url">--><?php //bloginfo('name'); ?><!--</a></h1>-->
+<!---->
+<!--                --><?php //else : ?>
+<!---->
+<!--                    <a class="navbar-brand" rel="home" href="--><?php //echo esc_url(home_url('/')); ?><!--"-->
+<!--                       title="--><?php //echo esc_attr(get_bloginfo('name', 'display')); ?><!--"-->
+<!--                       itemprop="url">--><?php //bloginfo('name'); ?><!--</a>-->
+<!---->
+<!--                --><?php //endif; ?>
+<!---->
+<!---->
+<!--                    --><?php //} else {
+//                        the_custom_logo();
+//                    } ?><!-- end custom logo -->
+<!---->
+<!--                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"-->
+<!--                            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--                        <span class="navbar-toggler-icon"></span>-->
+<!--                    </button>-->
+<!---->
                     <!-- The WordPress Menu goes here -->
-                    <?php wp_nav_menu(
-                        array(
-                            'theme_location' => 'primary',
-                            'container_class' => 'collapse navbar-collapse',
-                            'container_id' => 'navbarNavDropdown',
-                            'menu_class' => 'navbar-nav ml-auto',
-                            'fallback_cb' => '',
-                            'menu_id' => 'main-menu',
-                            'depth' => 2,
-                            'walker' => new Understrap_WP_Bootstrap_Navwalker(),
-                        )
-                    ); ?>
-                    <?php if ('container' == $container) : ?>
-                </div><!-- .container -->
-            <?php endif; ?>
-
-            </nav><!-- .site-navigation -->
-
-        </div><!-- #wrapper-navbar end -->
+<!--                    --><?php //wp_nav_menu(
+//                        array(
+//                            'theme_location' => 'primary',
+//                            'container_class' => 'collapse navbar-collapse',
+//                            'container_id' => 'navbarNavDropdown',
+//                            'menu_class' => 'navbar-nav ml-auto',
+//                            'fallback_cb' => '',
+//                            'menu_id' => 'main-menu',
+//                            'depth' => 2,
+//                            'walker' => new Understrap_WP_Bootstrap_Navwalker(),
+//                        )
+//                    ); ?>
+<!--                    --><?php //if ('container' == $container) : ?>
+<!--                </div>--><!-- .container -->
+<!--            --><?php //endif; ?>
+<!---->
+<!--            </nav>--><!-- .site-navigation -->
+<!---->
+<!--        </div>--><!-- #wrapper-navbar end -->
 
         <!-- ******************* Slider ******************* -->
 
@@ -184,7 +158,7 @@ $container = get_theme_mod('understrap_container_type');
         <!--Display slider with images taken from posts in that category if any of these pages indicated by slug-->
         <?php if (is_front_page() || in_array($post_slug, $slider_slugs)) : ?>
             <?php get_template_part('slider'); ?>
-        <?php else: ?>
+        <?php elseif (has_post_thumbnail()) : ?>
             <!--Static image otherwise-->
             <div class="header-image-fullscreen">
                 <?php the_post_thumbnail('full'); ?>
@@ -193,6 +167,3 @@ $container = get_theme_mod('understrap_container_type');
 
     </div> <!-- header -->
 </div>
-
-
-
