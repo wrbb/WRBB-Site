a3fad9ddb43e52e4f7344674edf821281e4ae03c
Chris Sims <chrissims@Chriss-MacBook-Pro.local>
2020-09-26 19:11:33 -0400

    animated search bar fix

--- searchform.php
+++ searchform.php
@@ -6,19 +6,22 @@
  */
 
 ?>
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label class="assistive-text" for="s">
-<!--        --><?php //esc_html_e( 'Search', 'understrap' ); ?>
-    </label>
-	<div class="input-group">
+<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+<!--	<label class="assistive-text" for="s">-->
+<!---->
+<!--    </label>-->
+	<div class="d-inline-flex" id="search-container">
 
 		<input class="field form-control" id="s" name="s" type="text"
 			placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="<?php the_search_query(); ?>">
         <i class="fa fa-search"></i>
 
+
+
         <span class="input-group-append">
 <!--			<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit"-->
 <!--			value="--><?php //esc_attr_e( 'Search', 'understrap' ); ?><!--">-->
 	    </span>
+
 	</div>
 </form>
