7f0ced12d84247c41f1f2286a879a449b9436847
Spencer LaChance <lachance.s@husky.neu.edu>
2020-12-27 18:31:54 -0500

    Added logic to switch between Radiojar and Icecast players

--- src/js/wrbb-js/playerbar.js
+++ src/js/wrbb-js/playerbar.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(() => {
+    let isRadiojar = true;
+    let popupWidth = isRadiojar ? 650 : 300;
+    let popupHeight = isRadiojar ? 300 : 150;
+    let openFeatures = 'status=no,toolbar=no,scrollbars=no,titlebar=no,menubar=no,width=' +
+        popupWidth + ',height=' + popupHeight + ',directories=no,location=no';
+
     jQuery(".play-button--toast").on("click", () => {
-        window.open('/playerbar-popup', 'win2',
-            'status=no,toolbar=no,scrollbars=no,titlebar=no,menubar=no,width=650,height=302,' +
-            'directories=no,location=no');
+        window.open('/playerbar-popup', 'win2', openFeatures);
     });
 });
 
--- src/wrbb-templates/playerbar-popup.php
+++ src/wrbb-templates/playerbar-popup.php
@@ -13,6 +13,8 @@ $show = $result['items'][0]['title'];
 
 $play_button = '<i class="fa fa-play-circle fa-2x"></i>';
 $pause_button = '<i class="fa fa-pause-circle fa-2x"></i>';
+
+$is_radiojar = true;
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +33,9 @@ $pause_button = '<i class="fa fa-pause-circle fa-2x"></i>';
 	<?php wp_head(); ?>
 </head>
 <body>
+
+<?php if ($is_radiojar) : ?>
+
 <div id="rj-player">
  <div class="player-v3 player-medium">
   <div id="rj-cover">
@@ -93,5 +98,99 @@ $pause_button = '<i class="fa fa-pause-circle fa-2x"></i>';
    }
  }
 </script>
+
+<?php else : ?>
+
+<script type="text/javascript">
+    let playing = false;
+
+    jQuery(document).ready(() => {
+        const audio = new Audio('http://129.10.161.130:8000/');
+
+        let setAudioLevel = (audioLevel) => {
+            if (audioLevel > 1 || audioLevel < 0) {
+                return;
+            }
+            audio.volume = audioLevel;
+        };
+
+        let playAudio = () => {
+            playing = true;
+            setAudioLevel(document.getElementById('volume-slider').value / 100);
+            document.getElementById('popup-button').innerHTML = '<?php echo $pause_button ?>'
+        };
+
+        let pauseAudio = () => {
+            playing = false;
+            setAudioLevel(0);
+            document.getElementById('popup-button').innerHTML = '<?php echo $play_button ?>';
+        };
+
+        let toggleAudio = () => {
+            if (playing) {
+                pauseAudio();
+            } else {
+                playAudio();
+            }
+        };
+
+        let promise = audio.play();
+        if (promise !== undefined) {
+            promise.then(() => {
+                playAudio();
+            }).catch(() => {
+                window.close();
+            });
+        } else {
+            window.close();
+        }
+
+        jQuery('.play-button--popup').on('click', () => {
+            toggleAudio();
+        });
+
+        jQuery(document).on('keydown', event => {
+            if (event.keyCode === 32) {
+                toggleAudio();
+            }
+        });
+
+        jQuery('#volume-slider').on('change', () => {
+            let val = jQuery('#volume-slider').val();
+            console.log(val);
+            setAudioLevel(val / 100);
+        });
+    });
+</script>
+
+<div class="playerbar playerbar--popup">
+    <p class="current-show">
+        <strong>Current Show:</strong>
+        <br>
+			<?php echo $show ?>
+    </p>
+    <div class="player-controls">
+        <div class="player-play-pause">
+            <a class="play-button--popup" id="popup-button">
+							<?php echo $play_button ?>
+            </a>
+        </div>
+        <div class="volume-controls">
+            <i id="volume-icon" class="fa fa-volume-up fa-lg" aria-hidden="true"></i>
+            <label for='volume-slider'></label>
+            <input
+                    id='volume-slider'
+                    type="range"
+                    min="1"
+                    max="100"
+                    step="1"
+                    value="50"
+            >
+        </div>
+    </div>
+</div>
+
+<?php endif ?>
+
 </body>
 </html>
