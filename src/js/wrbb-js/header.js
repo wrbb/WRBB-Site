6f832f21c925d567021e70315200f207cc5d2a62
Spencer LaChance <lachance.s@husky.neu.edu>
2021-01-14 12:23:09 -0500

    Changed the header logic so it is opened using pure CSS instead of JS

--- src/js/wrbb-js/header.js
+++ src/js/wrbb-js/header.js
@@ -2,13 +2,12 @@ let isMobile = window.matchMedia("only screen and (max-width: 992px)").matches;
 
 jQuery(".nav-item").on({
     mouseenter: function () {
-        // Open dropdown
+        // Dropdown was opened
         let dropdown_id = this.id;
-        jQuery(".dropdown-toggle#" + dropdown_id).toggleClass("selected");
-        jQuery(".nav-item#" + dropdown_id).css("background-color", "#d30f0f");
-        jQuery(".dropdown-content#" + dropdown_id).css("display", "inherit");
         if (isMobile) {
-            // Wait 100 ms before restoring the link to prevent interference with the
+            // Restore the dropdown title links because they are disabled until the user
+            // opens the dropdown on mobile.
+            // Wait 100 ms before doing so to prevent interference with the
             // opening of the dropdown.
             setTimeout(() => {
                 jQuery(".dropdown-toggle#" + dropdown_id).css("pointer-events", "auto");
@@ -16,30 +15,23 @@ jQuery(".nav-item").on({
         }
     },
     mouseleave: function () {
-        // Close dropdown
+        // Dropdown was closed
         let dropdown_id = this.id;
-        jQuery(".dropdown-toggle#" + dropdown_id).toggleClass("selected");
-        jQuery(".nav-item#" + dropdown_id).css("background-color", "transparent");
-        // Wait 10 ms before closing the dropdown.
-        // This fixes some funny behavior on mobile.
-        setTimeout(() => {
-            jQuery(".dropdown-content#" + dropdown_id).css("display", "none");
-        }, 10);
         if (isMobile) jQuery(".dropdown-toggle#" + dropdown_id).css("pointer-events", "none");
     }
 });
 
 // On mobile, toggle the header nav with the hamburger button
 jQuery(".navbar-toggler").click(function () {
-    jQuery(".wrbb-menu").toggleClass("selected");
-    jQuery(".wrbb-navbar").toggleClass("selected");
+    jQuery(".wrbb-menu").toggleClass("nav-open");
+    jQuery(".wrbb-navbar").toggleClass("nav-open");
 });
 
 if (isMobile) {
     // Close the header nav when the user clicks outside of it
     jQuery(document).click(function () {
-        jQuery(".wrbb-menu").removeClass("selected");
-        jQuery(".wrbb-navbar").removeClass("selected");
+        jQuery(".wrbb-menu").removeClass("nav-open");
+        jQuery(".wrbb-navbar").removeClass("nav-open");
     })
     // Don't close the nav when the user clicks inside of it
     jQuery(".header").click(function (e) {
