d384662481662dcd4e788288660cc091db3ef69b
Spencer LaChance <lachance.s@husky.neu.edu>
2021-03-24 11:32:32 -0400

    Fixed the default page template to match the rest of the site design

--- page.php
+++ page.php
@@ -20,34 +20,34 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
+        <hr class="header-line">
+
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+            <div class="col-md-12 content-area" id="primary">
 
-			<main class="site-main" id="main">
+                <main class="site-main" id="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+                        <?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
+                        <?php
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
+                        ?>
 
-				<?php endwhile; // end of the loop. ?>
+                    <?php endwhile; // end of the loop. ?>
 
-			</main><!-- #main -->
+                </main><!-- #main -->
 
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+            </div><!-- #primary -->
 
-	</div><!-- .row -->
+	    </div><!-- .row -->
 
-</div><!-- Container end -->
+    </div><!-- Container end -->
 
 </div><!-- Wrapper end -->
 
