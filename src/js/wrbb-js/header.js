6d46983e87dcf871559db0b5002bc5c2708990d8
WRBB Tech Team <brycethuilot@ccs.neu.edu>
2021-08-28 11:41:36 -0400

    hackjob during transfer

--- src/js/wrbb-js/header.js
+++ src/js/wrbb-js/header.js
@@ -15,41 +15,41 @@ jQuery(document).ready(() => {
             e.stopPropagation();
         })
     }
-});
+	
+	jQuery(".nav-item").on({
+		mouseenter: function () {
+			// Dropdown was opened
+			let dropdown_id = this.id;
+			jQuery(".nav-item#" + dropdown_id).css("background-color", "#d30f0f");
+			jQuery(".dropdown-content#" + dropdown_id).show("fast");
+			if (isMobile) {
+				// Restore the dropdown title links because they are disabled until the user
+				// opens the dropdown on mobile.
+				// Wait 100 ms before doing so to prevent interference with the
+				// opening of the dropdown.
+				setTimeout(() => {
+					jQuery(".dropdown-toggle#" + dropdown_id).css("pointer-events", "auto");
+				}, 100);
+			}
+		},
+		mouseleave: function () {
+			// Dropdown was closed
+			let dropdown_id = this.id;
+			jQuery(".nav-item#" + dropdown_id).css("background-color", "inherit");
+			jQuery(".dropdown-content#" + dropdown_id).hide("fast");
+			if (isMobile) {
+				jQuery(".dropdown-toggle#" + dropdown_id).css("pointer-events", "none");
+			}
+		}
+	});
 
-jQuery(".nav-item").on({
-    mouseenter: function () {
-        // Dropdown was opened
-        let dropdown_id = this.id;
-        jQuery(".nav-item#" + dropdown_id).css("background-color", "#d30f0f");
-        jQuery(".dropdown-content#" + dropdown_id).show("fast");
-        if (isMobile) {
-            // Restore the dropdown title links because they are disabled until the user
-            // opens the dropdown on mobile.
-            // Wait 100 ms before doing so to prevent interference with the
-            // opening of the dropdown.
-            setTimeout(() => {
-                jQuery(".dropdown-toggle#" + dropdown_id).css("pointer-events", "auto");
-            }, 100);
-        }
-    },
-    mouseleave: function () {
-        // Dropdown was closed
-        let dropdown_id = this.id;
-        jQuery(".nav-item#" + dropdown_id).css("background-color", "inherit");
-        jQuery(".dropdown-content#" + dropdown_id).hide("fast");
-        if (isMobile) {
-            jQuery(".dropdown-toggle#" + dropdown_id).css("pointer-events", "none");
-        }
-    }
-});
+	// On mobile, toggle the header nav with the hamburger button
+	jQuery(".navbar-toggler").click(function () {
+		jQuery(".wrbb-menu").toggleClass("nav-open");
+		jQuery(".wrbb-navbar").toggleClass("nav-open");
+	});
 
-// On mobile, toggle the header nav with the hamburger button
-jQuery(".navbar-toggler").click(function () {
-    jQuery(".wrbb-menu").toggleClass("nav-open");
-    jQuery(".wrbb-navbar").toggleClass("nav-open");
-});
-
-jQuery(".fa-times").click(function () {
-    jQuery(".maintenance-banner").hide();
+	jQuery(".fa-times").click(function () {
+		jQuery(".maintenance-banner").hide();
+	});
 });
