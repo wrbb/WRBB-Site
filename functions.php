f49b54700caeb552559f1d224ae00f269776b078
Spencer LaChance <lachance.s@husky.neu.edu>
2021-08-28 13:22:12 -0400

    Single template function formatting

--- functions.php
+++ functions.php
@@ -98,21 +98,18 @@ add_filter('single_template', 'my_single_template');
 /**
  * Single template function which will choose our template
  */
-function my_single_template()
-{
-	/**
-	 * Checks for single template by category
-	 * Check by category slug and ID
-	 */
-	foreach ((array)get_the_category() as $cat) :
-
-		if (file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
-			return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
-
-    elseif (file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
-			return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
-
-	endforeach;
+function my_single_template() {
+    /**
+     * Checks for single template by category
+     * Check by category slug and ID
+     */
+    foreach ((array)get_the_category() as $cat) {
+        if ( file_exists( SINGLE_PATH . '/single-cat-' . $cat->slug . '.php' ) ) {
+            return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
+        } elseif ( file_exists( SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php' ) ) {
+            return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
+        }
+    }
 }
 
 /**
