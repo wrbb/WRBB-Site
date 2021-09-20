980631a38b2b8e3c446914a4c2fd525d5d3ad173
Spencer LaChance <lachance.s@husky.neu.edu>
2021-09-20 18:33:47 -0400

    Added volume icon back + rearranged things a bit

--- src/wrbb-templates/playerbar.php
+++ src/wrbb-templates/playerbar.php
@@ -8,12 +8,17 @@
 ?>
 
 <div class="playerbar">
-    <h2 id="player-header">Listen Live</h2>
     <div class="player-controls">
-        <div class="play-button">
-            <i class="fas fa-play-circle fa-lg"></i>
+        <div>
+            <h3 class="player-text">Listen Live</h3>
+            <div class="play-button">
+                <i class="fas fa-play-circle fa-lg"></i>
+            </div>
         </div>
-        <label for='volume-slider'></label>
-        <input id='volume-slider' type="range" min="1" max="100" step="1" value="50">
+        <div>
+            <i class="fa fa-volume-up fa-md" aria-hidden="true"></i>
+            <label for='volume-slider'></label>
+            <input id='volume-slider' type="range" min="1" max="100" step="1" value="50">
+        </div>
     </div>
 </div>
